711bdd6519ae63580648b04a38659060e01fd98a
JMX <jesusmxyx@gmail.com>
2026-09-02 09:53:16 -0400

    Charge the menu config

--- .config/labwc/menu.sh
+++ .config/labwc/menu.sh
@@ -4,7 +4,7 @@ printf '%b\n' '
 
   <item label="Web Browser" name.action="Execute" command.action="waterfox" icon="waterfox" />
   <item label="Terminal" name.action="Execute" command.action="foot" icon="utilities-terminal" />
-  <item label="File Manager" name.action="Execute" command.action="pcmanfm" icon="system-file-manager" />
+  <item label="File Manager" name.action="Execute" command.action="caja" icon="system-file-manager" />
   <item label="Tweaks" name.action="Execute" command.action="labwc-tweaks" icon="configure" />
 
   <separator />'
@@ -15,8 +15,8 @@ printf '%b\n' '
   <separator />
 
   <menu id="Preferences" label="Preferences" icon="applications-engineering">
-    <item label="Edit rc.xml" name.action="Execute" command.action="greany ~/.config/labwc/rc.xml" icon="text-x-generic" />
-    <item label="Edit autostart" name.action="Execute" command.action="greany ~/.config/labwc/autostart" icon="text-x-generic" />
+    <item label="Edit rc.xml" name.action="Execute" command.action="foot -e nano ~/.config/labwc/rc.xml" icon="text-x-generic" />
+    <item label="Edit autostart" name.action="Execute" command.action="foot -e nano ~/.config/labwc/autostart" icon="text-x-generic" />
   </menu>
 
   <menu id="Exit" label="Exit" icon="application-exit">
